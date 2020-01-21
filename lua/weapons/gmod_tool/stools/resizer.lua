025865a23d4a7e4559580950d438c4d654316d66
Cheezus <rcheeze1337@gmail.com>
2020-01-21 12:48:23 -0500

    Clamped convars, added copy size function

--- lua/weapons/gmod_tool/stools/resizer.lua
+++ lua/weapons/gmod_tool/stools/resizer.lua
@@ -3,15 +3,15 @@ TOOL.Name = "#Resizer"
 TOOL.Command = nil
 TOOL.ConfigName = ""
 
-if (CLIENT) then
+if CLIENT then
     language.Add("Tool.resizer.name", "Resizer")
     language.Add("Tool.resizer.desc", "Resizes props/ragdolls/NPCs")
-    language.Add("Tool.resizer.0", "Click on an object to resize it. Right click to reset.")
-    CreateClientConVar("resizer_xsize", "2", false, true)
-    CreateClientConVar("resizer_ysize", "2", false, true)
-    CreateClientConVar("resizer_zsize", "2", false, true)
-    CreateClientConVar("resizer_xyzsize", "2", false, false)
-    CreateClientConVar("resizer_allbones", "0", false, true)
+    language.Add("Tool.resizer.0", "Click on an object to resize it. Right click to copy size. Reload to reset to default size.")
+    CreateClientConVar("resizer_xsize", "1", false, true, "", -50, 50)
+    CreateClientConVar("resizer_ysize", "1", false, true, "", -50, 50)
+    CreateClientConVar("resizer_zsize", "1", false, true, "", -50, 50)
+    CreateClientConVar("resizer_xyzsize", "1", false, false, "", -50, 50)
+    CreateClientConVar("resizer_allbones", "0", false, true, "", 0, 1)
 
     local function _resizer_xyzcallback(cvar, prevValue, newValue)
         RunConsoleCommand("resizer_xsize", newValue)
@@ -20,14 +20,55 @@ if (CLIENT) then
     end
 
     cvars.AddChangeCallback("resizer_xyzsize", _resizer_xyzcallback)
+
+    net.Receive("resizer_getsize", function()
+        local scale = net.ReadVector()
+        RunConsoleCommand("resizer_xsize", scale.x)
+        RunConsoleCommand("resizer_ysize", scale.y)
+        RunConsoleCommand("resizer_zsize", scale.z)
+    end)
 end
 
-function TOOL:RightClick(trace)
-    if (trace.HitNonWorld and trace.Entity ~= nil and trace.Entity ~= 0) then
-        --props | ragdolls | npcs
-        if SERVER and (trace.Entity:GetClass() == "prop_physics" or trace.Entity:GetClass() == "prop_ragdoll" or trace.Entity:IsNPC()) then
-            for i = 0, trace.Entity:GetBoneCount() do
-                trace.Entity:ManipulateBoneScale(i, Vector(1, 1, 1))
+if SERVER then
+    util.AddNetworkString("resizer_getsize")
+end
+
+local function resize(Player, Entity, Data)
+    if not SERVER then return end
+    if not Entity:IsValid() then return end
+
+    for i = 0, Entity:GetBoneCount() do
+        Entity:ManipulateBoneScale(i, Data.propResizerSize)
+    end
+
+    Entity.propResizerSize = Data.propResizerSize
+    duplicator.StoreEntityModifier(Entity, "PropResizerData", Data)
+end
+
+duplicator.RegisterEntityModifier("PropResizerData", resize)
+
+function TOOL:LeftClick(trace)
+    if trace.HitNonWorld and trace.Entity ~= nil and trace.Entity ~= 0 then
+        if SERVER then
+            local scale = Vector(tonumber(self:GetOwner():GetInfo("resizer_xsize")), tonumber(self:GetOwner():GetInfo("resizer_ysize")), tonumber(self:GetOwner():GetInfo("resizer_zsize")))
+
+            --props
+            if trace.Entity:GetClass() == "prop_physics" then
+                resize(self:GetOwner(), trace.Entity, {
+                    propResizerSize = scale
+                })
+            end
+
+            --ragdolls and npcs
+            if trace.Entity:GetClass() == "prop_ragdoll" or trace.Entity:IsNPC() then
+                if tonumber(self:GetOwner():GetInfo("resizer_allbones")) > 0 then
+                    resize(self:GetOwner(), trace.Entity, {
+                        propResizerSize = scale
+                    })
+                else
+                    local Bone = trace.Entity:TranslatePhysBoneToBone(trace.PhysicsBone)
+                    trace.Entity:ManipulateBoneScale(Bone, scale)
+                end
             end
         end
 
@@ -37,29 +78,30 @@ function TOOL:RightClick(trace)
     return false
 end
 
-function TOOL:LeftClick(trace)
-    if (trace.HitNonWorld and trace.Entity ~= nil and trace.Entity ~= 0) then
-        if (SERVER) then
-            local scale = Vector(tonumber(self:GetOwner():GetInfo("resizer_xsize")), tonumber(self:GetOwner():GetInfo("resizer_ysize")), tonumber(self:GetOwner():GetInfo("resizer_zsize")))
+function TOOL:RightClick(trace)
+    if trace.HitNonWorld and trace.Entity ~= nil and trace.Entity ~= 0 then
+        if SERVER and trace.Entity:GetClass() == "prop_physics" and trace.Entity.propResizerSize then
+            local scale = trace.Entity.propResizerSize
+            net.Start("resizer_getsize")
+            net.WriteVector(scale)
+            net.Send(self:GetOwner())
+        end
 
-            --props
-            if (trace.Entity:GetClass() == "prop_physics") then
-                for i = 0, trace.Entity:GetBoneCount() do
-                    trace.Entity:ManipulateBoneScale(i, scale)
-                end
+        return true
+    end
+
+    return false
+end
+
+function TOOL:Reload(trace)
+    if trace.HitNonWorld and trace.Entity ~= nil and trace.Entity ~= 0 then
+        if SERVER and (trace.Entity:GetClass() == "prop_physics" or trace.Entity:GetClass() == "prop_ragdoll" or trace.Entity:IsNPC()) then
+            for i = 0, trace.Entity:GetBoneCount() do
+                trace.Entity:ManipulateBoneScale(i, Vector(1, 1, 1))
             end
 
-            --ragdolls and npcs
-            if (trace.Entity:GetClass() == "prop_ragdoll" or trace.Entity:IsNPC()) then
-                if (tonumber(self:GetOwner():GetInfo("resizer_allbones")) > 0) then
-                    for i = 0, trace.Entity:GetBoneCount() do
-                        trace.Entity:ManipulateBoneScale(i, scale)
-                    end
-                else
-                    local Bone = trace.Entity:TranslatePhysBoneToBone(trace.PhysicsBone)
-                    trace.Entity:ManipulateBoneScale(Bone, scale)
-                end
-            end
+            trace.Entity.propResizerSize = nil
+            duplicator.ClearEntityModifier(trace.Entity, "PropResizerData")
         end
 
         return true
